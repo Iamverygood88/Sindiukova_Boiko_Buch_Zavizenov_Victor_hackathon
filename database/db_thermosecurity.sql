adcab16f6d0803052d753bffaba638a8083da23b
Iamverygood88 <e_sindiukova@fanshaweonline.ca>
2019-10-07 21:29:19 -0400

    try

--- database/db_thermosecurity.sql
+++ database/db_thermosecurity.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Oct 07, 2019 at 07:53 PM
+-- Generation Time: Oct 07, 2019 at 09:28 PM
 -- Server version: 5.7.25
 -- PHP Version: 7.3.1
 
@@ -36,7 +36,8 @@ CREATE TABLE `tbl_interface` (
 --
 
 INSERT INTO `tbl_interface` (`id`, `Image`) VALUES
-(1, 'corgi.jpg');
+(1, 'corgi.jpg'),
+(2, 'Welchcorgipembroke.jpg');
 
 -- --------------------------------------------------------
 
@@ -56,9 +57,9 @@ CREATE TABLE `tbl_sections` (
 --
 
 INSERT INTO `tbl_sections` (`id`, `Heading`, `Paragraph`, `Content`) VALUES
-(1, 'Control your environment like never before', 'Manage all electronics in your smart home from one device. \r\n', ''),
-(2, 'Easy to use', 'Intuitive interface with light elements in a touch bar, that displays all necessary information. ', ''),
-(3, 'Safe for kids', 'Face recognition camera and touch ID secures the access to the device. \r\n', 'corgi.jpg');
+(1, 'Control your environment like never before', 'Manage all electronics in your smart home from one device. \r\n', 'Welchcorgipembroke.jpg'),
+(2, 'Easy to use', 'Intuitive interface with light elements in a touch bar, that displays all necessary information. ', 'Welchcorgipembroke.jpg'),
+(3, 'Safe for kids', 'Face recognition camera and touch ID secures the access to the device. \r\n', 'Welchcorgipembroke.jpg');
 
 --
 -- Indexes for dumped tables
@@ -84,13 +85,13 @@ ALTER TABLE `tbl_sections`
 -- AUTO_INCREMENT for table `tbl_interface`
 --
 ALTER TABLE `tbl_interface`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `tbl_sections`
 --
 ALTER TABLE `tbl_sections`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
