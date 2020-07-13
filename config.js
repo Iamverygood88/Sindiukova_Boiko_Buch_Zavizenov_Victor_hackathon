8405da20a80b382160e66641ea5f76362743a408
Evgeniia Sindiukova <43250424+Iamverygood88@users.noreply.github.com>
2020-07-13 18:25:26 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config = {
     uname:"b9f297d586a0e8",
     upass: "7e73e473", // blank for windows
-    host: "us-cdbr-east-02.cleardb.net",
+    host: "us-cdbr-east-02.cleardb.com",
     db: "heroku_ba4f83854fd98d2"
 }
 
