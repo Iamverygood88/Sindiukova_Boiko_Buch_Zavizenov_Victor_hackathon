a4cc3c9615abdad0a19d58ae25f1d76ef4e8cc2e
Evgeniia Sindiukova <43250424+Iamverygood88@users.noreply.github.com>
2020-07-13 01:08:54 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,8 @@
 const config = {
-    uname:"root",
-    upass: "root", // blank for windows
-    port: 8889, //3306 for windows
-    host: 'localhost',
-    db: 'db_thermosecurity'
+    uname:"b9f297d586a0e8",
+    upass: "7e73e473", // blank for windows
+    host: 'us-cdbr-east-02.cleardb.com',
+    db: 'heroku_ba4f83854fd98d2'
 }
 
 module.exports = config; 
