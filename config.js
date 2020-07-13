2788ab0b6f593b5d83f724c85fdeb35cd67a82a7
Evgeniia Sindiukova <43250424+Iamverygood88@users.noreply.github.com>
2020-07-13 18:13:20 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const config = {
     uname:"b9f297d586a0e8",
     upass: "7e73e473", // blank for windows
-    host: 'us-cdbr-east-02.cleardb.com',
-    db: 'heroku_ba4f83854fd98d2'
+    host: "us-cdbr-east-02.cleardb.net",
+    db: "heroku_ba4f83854fd98d2"
 }
 
 module.exports = config; 
